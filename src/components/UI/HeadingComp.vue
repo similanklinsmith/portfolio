80587a1ccf47c35c0563b9d4721361bfc1fedce6
unknown <similan.klinsmith@mail.kmutt.ac.th>
2023-05-30 14:18:14 +0700

    [ADD] HeadingComp

---
+++ src/components/UI/HeadingComp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="heading">
+    <div class="heading-M">{{ heading }}</div>
+    <div class="body-L">
+      {{ subheading }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeadingComp",
+  props: ["heading", "subheading"],
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/colors/colors.scss";
+.heading {
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.8rem;
+  .heading-M {
+    color: $heading;
+  }
+  .body-L {
+    color: $subHeading;
+  }
+}
+</style>
