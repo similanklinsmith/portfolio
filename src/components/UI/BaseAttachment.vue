662fadc6d0fc48a1bbaca2e555afb85845d4d616
unknown <similan.klinsmith@mail.kmutt.ac.th>
2023-06-04 17:08:33 +0700

    [ADD] BaseAttachment

---
+++ src/components/UI/BaseAttachment.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="attachment body-M" @click="onClick">
+    <div class="leading">
+      <div class="leading-icon">
+        <font-awesome-icon :icon="`fa-solid fa-${leadingIcon}`" />
+      </div>
+      <div class="content">{{ content }}</div>
+    </div>
+    <div class="trailing-icon" v-if="isTrailing">
+      <font-awesome-icon icon="fa-solid fa-arrow-down" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseAttachment",
+  props: {
+    isTrailing: Boolean,
+    leadingIcon: String,
+    content: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit("onClick");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/colors/colors.scss";
+.attachment {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  transition: 0.3s all ease-in-out;
+  cursor: pointer;
+  .leading {
+    display: flex;
+    column-gap: 1.2rem;
+    align-items: center;
+    .leading-icon {
+      height: 4rem;
+      width: 4rem;
+      min-width: 4rem;
+      border-radius: 0.8rem;
+      background-color: $light2;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: $description;
+      transition: 0.3s all ease-in-out;
+    }
+    .content {
+      transition: 0.3s all ease-in-out;
+      color: $mainText;
+    }
+  }
+  .trailing-icon {
+    color: $primaryMain;
+  }
+  &:hover {
+    .leading {
+      .leading-icon {
+        background-color: $secondary4;
+        color: $secondaryMain;
+      }
+      .content {
+        color: $secondaryMain;
+      }
+    }
+  }
+}
+</style>
