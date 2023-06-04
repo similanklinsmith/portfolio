27226437c341c999305fba76810bddf9001d7050
unknown <similan.klinsmith@mail.kmutt.ac.th>
2023-06-04 17:08:54 +0700

    [ADD] BaseLine

---
+++ src/components/UI/BaseLine.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="weight" />
+</template>
+
+<script>
+export default {
+  name: "BaseLine",
+  props: ["weight"],
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/colors/colors.scss";
+.thin {
+  height: 0.01rem;
+  background: $light1;
+}
+.normal {
+  height: 0.02rem;
+  background: $light1;
+}
+.bold {
+  height: 0.03rem;
+  background: $light1;
+}
+</style>
