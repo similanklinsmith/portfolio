ff7113b5e913dcd6b9f2701d1433ff086edb99c0
unknown <similan.klinsmith@mail.kmutt.ac.th>
2023-06-09 14:23:03 +0700

    [ADD] download and open documents

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -48,8 +48,7 @@
       </div>
       <div class="about-me">
         <div class="label-L">{{ text["about"]["aboutLeading"] }}</div>
-        <div class="body-M">{{ text["about"]["aboutContent"] }}
-        </div>
+        <div class="body-M">{{ text["about"]["aboutContent"] }}</div>
       </div>
       <div class="personal-information">
         <div class="label-L">{{ text["about"]["personalLeading"] }}</div>
@@ -58,24 +57,33 @@
             <div class="leading-icon body-M">
               <font-awesome-icon icon="fa-solid fa-envelope" />
             </div>
-            <KeyValueComp :leading="text['about']['emailLeading']" :value="'deep25952@gmail.com'" />
+            <KeyValueComp
+              :leading="text['about']['emailLeading']"
+              :value="'deep25952@gmail.com'"
+            />
           </div>
           <div class="info">
             <div class="leading-icon body-M">
               <font-awesome-icon icon="fa-solid fa-mobile" />
             </div>
-            <KeyValueComp :leading="text['about']['phoneLeading']" :value="'063-598-3619'" />
+            <KeyValueComp
+              :leading="text['about']['phoneLeading']"
+              :value="'063-598-3619'"
+            />
           </div>
         </div>
         <div class="info">
           <div class="leading-icon body-M">
             <font-awesome-icon icon="fa-solid fa-house" />
           </div>
-          <KeyValueComp :leading="text['about']['locationLeading']" :value="text['about']['locationValue']" />
+          <KeyValueComp
+            :leading="text['about']['locationLeading']"
+            :value="text['about']['locationValue']"
+          />
         </div>
       </div>
       <div class="education">
-        <div class="label-L">{{ text['about']['educationLeading'] }}</div>
+        <div class="label-L">{{ text["about"]["educationLeading"] }}</div>
         <div class="info">
           <div class="leading-icon body-M">
             <font-awesome-icon icon="fa-solid fa-envelope" />
@@ -89,60 +97,96 @@
           <div class="leading-icon body-M">
             <font-awesome-icon icon="fa-solid fa-scroll" />
           </div>
-          <KeyValueComp :leading="text['about']['GPAXLeading']" :value="text['about']['GPAXValue']" />
+          <KeyValueComp
+            :leading="text['about']['GPAXLeading']"
+            :value="text['about']['GPAXValue']"
+          />
         </div>
       </div>
     </div>
     <div class="col-2">
       <div class="resume">
-        <div class="label-L">{{ text['about']['resumeLeading'] }}</div>
+        <div class="label-L">{{ text["about"]["resumeLeading"] }}</div>
         <BaseAttachment
+          @onClick="downloadWithAxios(file.src, file.title)"
           :isTrailing="true"
           :leadingIcon="'file-lines'"
           :content="'Resume.pdf'"
         />
       </div>
       <div class="certificates">
-        <div class="label-L">{{ text['about']['certificateLeading'] }}</div>
+        <div class="label-L">{{ text["about"]["certificateLeading"] }}</div>
         <BaseAttachment
+          @onClick="null"
+          :isActive="false"
           :isTrailing="false"
           :leadingIcon="'link'"
           :content="'TETET(English) - Level B2'"
         />
         <BaseLine :weight="'thin'" />
         <BaseAttachment
+          @onClick="
+            openLink(
+              'https://drive.google.com/file/d/1GfOJw2wnyhctdHKH2YLFq3RT7lXBLoH4/view?usp=sharing'
+            )
+          "
           :isTrailing="false"
           :leadingIcon="'link'"
           :content="'PLIDA(ITALIAN) - Level B1'"
         />
         <BaseLine :weight="'thin'" />
         <BaseAttachment
+          @onClick="
+            openLink(
+              'https://drive.google.com/file/d/1M63J-hk3BwEEB9zkQV93rdJMQhjHl1za/view?usp=drive_link'
+            )
+          "
           :isTrailing="false"
           :leadingIcon="'link'"
           :content="'UDEMY : Build Responsive Real-World Websites with HTML and CSS'"
         />
         <BaseLine :weight="'thin'" />
         <BaseAttachment
+          @onClick="
+            openLink(
+              'https://drive.google.com/file/d/1v3F7Ig3WbrpgayFbclXrkQf9ci4NZEhK/view?usp=drive_link'
+            )
+          "
           :isTrailing="false"
           :leadingIcon="'link'"
           :content="'UDEMY : Figma UI UX Design Essential'"
         />
         <BaseLine :weight="'thin'" />
         <BaseAttachment
+          @onClick="
+            openLink(
+              'https://drive.google.com/file/d/1Bb_sZlMhukA9ah2NRG-s79QrqblFr2Gl/view?usp=drive_link'
+            )
+          "
           :isTrailing="false"
           :leadingIcon="'link'"
           :content="'Ajinomoto Talent Scholarship'"
         />
         <BaseLine :weight="'thin'" />
         <BaseAttachment
+          @onClick="
+            openLink(
+              'https://drive.google.com/file/d/1h4cOCHwYo1MABhZ2WPm5-8fZ5inodHzm/view?usp=drive_link'
+            )
+          "
           :isTrailing="false"
           :leadingIcon="'link'"
           :content="'Ajinomoto Foundation Innovation Idea Contest 2022: Winner'"
         />
       </div>
       <div class="other">
-        <div class="label-L">{{ text['about']['otherLeading'] }}</div>
+        <div class="label-L">{{ text["about"]["otherLeading"] }}</div>
         <BaseAttachment
+          @onClick="
+            openLink(
+              'https://drive.google.com/drive/u/0/folders/1sM9UapU8SaZOje0kfWcRpmrFhaRskhA8'
+            )
+          "
           :isTrailing="false"
           :leadingIcon="'link'"
           :content="'Google Drive: Works'"
@@ -153,6 +197,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import BaseButton from "@/components/UI/BaseButton.vue";
 import KeyValueComp from "@/components/UI/KeyValueComp.vue";
 import BaseAttachment from "@/components/UI/BaseAttachment.vue";
@@ -163,12 +208,32 @@ export default {
   data() {
     return {
       lang: null,
-      text: null
-    }
+      text: null,
+      file: {
+        title: "Resume_Portfolio.pdf",
+        src: require("../assets/documents/resume_portfolio_Similan_Klinsmith.pdf"),
+      },
+    };
   },
   methods: {
     openLink(link) {
       window.open(link, "_blank");
+    },
+    downloadWithAxios(url, title) {
+      console.log(url);
+      axios
+        .get(url.default, {
+          responseType: "blob",
+        })
+        .then((response) => {
+          var fileURL = window.URL.createObjectURL(new Blob([response.data]));
+          var fileLink = document.createElement("a");
+          fileLink.href = fileURL;
+          fileLink.setAttribute("download", title);
+          document.body.appendChild(fileLink);
+          fileLink.click();
+        })
+        .catch(() => console.log(url));
     },
   },
   beforeMount() {
