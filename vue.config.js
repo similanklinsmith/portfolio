0e36a607147de500dc79aaf68a90d0e3d923ed88
unknown <similan.klinsmith@mail.kmutt.ac.th>
2023-06-09 14:21:45 +0700

    [FIX] handle binary pdf file

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  chainWebpack: (config) => {
+    config.module
+      .rule('pdf')
+      .test(/\.pdf$/)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: 'assets/[name].[hash:8].[ext]', // Adjust the output file name and path as needed
+      });
+  },
+};
+
