ff5f1abd2833b428bdbd135e60fc2dca0d40bb22
unknown <similan.klinsmith@mail.kmutt.ac.th>
2023-06-15 16:29:22 +0700

    [ADD] NotFound View

---
+++ src/views/exception/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="not-found-container">
+    
+  </div>
+</template>
+
+<script>
+export default {
+    name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/colors/colors.scss";
+
+</style>
